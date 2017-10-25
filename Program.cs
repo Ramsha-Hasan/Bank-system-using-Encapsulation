8cafbb486a793fadecc2cb5ab2b26b14b2a5ee83
Ramsha Hasan <ramsha.hasan2@gmail.com>
2017-10-25 19:12:56 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class BankSystem
+    {
+        private double balance;
+        private double deposit;
+        private double withdraw;
+
+        public double TBalance
+        {
+            get
+            {
+                return balance;
+            }
+
+            set
+            {
+                balance = value;
+            }
+        }
+
+        public double TDeposit
+        {
+            get
+            {
+                return deposit;
+            }
+
+            set
+            {
+                deposit = value;
+            }
+        }
+
+        public double TwithDraw
+        {
+            get
+            {
+                return withdraw;
+            }
+
+            set
+            {
+                withdraw = value;
+            }
+        }
+
+        public double Balance()
+        {
+            Console.Write("Enter your account balance ");
+            balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your account balance is {0}",balance);
+            return balance;
+        }
+
+
+        public double Deposit()
+        {
+            Console.Write("Enter your deposited amount ");
+            deposit = Convert.ToDouble(Console.ReadLine());
+            balance += deposit;
+            Console.WriteLine("Your account balance is {0}", balance);
+            return balance;
+        }
+
+
+        public double WithDraw()
+        {
+            Console.Write("Enter your withdrawn amount ");
+            withdraw = Convert.ToDouble(Console.ReadLine());
+            balance -= withdraw;
+            Console.WriteLine("Your account balance is {0}", balance);
+            return balance;
+        }
+    }
+   
+    
+    class Program : BankSystem
+    {
+        static void Main(string[] args)
+        {
+            BankSystem bank = new BankSystem();
+            bank.Balance();
+            bank.Deposit();
+            bank.WithDraw();
+            
+            Console.ReadKey();
+        }
+    }
+}
+
